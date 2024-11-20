fe7f89f129fa43755f313ee36a85564f4e8f0bca
Vast Dev <dev@getvast.app>
2024-11-19 19:27:28 -0800

    Changing social card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/vast-social-card.jpg",
     navbar: {
       // title: "Vast Docs",
       logo: {
